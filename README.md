0a5fce9906cb89d001bd1f86f2f6c179c64d7f71
nidhisingh11 <nidhi.nitdgp@gmail.com>
2021-07-23 20:40:22 +0530

    Check InputFile takes valid path or not

---
+++ README.md
@@ -0,0 +1,7 @@
+alt+enter = import the package
+ctrl+leftclick=correct the point
+# adazip
+
+It is a utility desktop application to compress and decompress multiple files and folders at a single time.
+The name adazip refers to advance zip files and folders.
+
--- src/main/java/org/zip/adazip/reader/InputReader.java
+++ src/main/java/org/zip/adazip/reader/InputReader.java
@@ -1,10 +1,24 @@
 package org.zip.adazip.reader;
 
 import java.io.*;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class InputReader {
+    //check the path is valid or not then add this valid in inputlist
+    public static boolean isValidFilePath(String path) {
+        File f = new File(path);
+        try {
+            f.getCanonicalPath();
+            return f.exists();
+        } catch (IOException e) {
+            return false;
+        } finally {
+            f = null;
+        }
+    }
 
     // get the user input file name ie. app_input.txt
     public List<String> readFile(String filePath) {
@@ -16,10 +30,18 @@ public class InputReader {
             try {
                 fr = new FileReader(file);
                 BufferedReader br = new BufferedReader(fr);  //creates a buffering character input stream
+                //each read path take as string (inside the path line by line )
+                String path;
+                while ((path = br.readLine()) != null) {
+                    boolean isPathExist = isValidFilePath(path) ;
+                    if(isPathExist){
+                        //inside txt file is exit then add in result output(inputFiles)
+                        inputFiles.add(path);
+                    }
+                    else{
+                        System.out.println("Input file is not correct " + path);
+                    }
 
-                String line;
-                while ((line = br.readLine()) != null) {
-                    inputFiles.add("input: " + line);
                 }
                 fr.close();    //closes the stream and release the resources
 
